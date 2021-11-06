7ee5858dd65efa9fdf5e78afe0de8f3542c6073b
jngsznn <sjiang@mit.edu>
2021-11-06 01:21:48 -0400

    search bar

--- App.js
+++ App.js
@@ -2,14 +2,63 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, Dimensions, ImageBackground } from 'react-native';
 import ImageZoom from 'react-native-image-pan-zoom'
-const d = Dimensions.get('window');
-export default function App() {
- return (
-   <View style={styles.container}>
-     
-        
+import {SearchBar} from 'react-native-elements';
 
-        <ImageZoom cropWidth={d.width+100}
+const d = Dimensions.get('window');
+export class StartBar extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = (search) => {this.setState({search});
+  };
+
+  render() {
+    const {search } = this.state;
+    const startPoint= search;
+
+    return (
+      <SearchBar
+        lightTheme="true"
+        input=""
+        placeholder="Start"
+        onChangeText={this.updateSearch}
+        value={search}
+      />
+    );
+  }
+}
+export class Destination extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = (search) => {this.setState({search});
+  };
+
+  render() {
+    const {search } = this.state;
+    const endPoint= search;
+
+    return (
+      <SearchBar
+        lightTheme="true"
+        input=""
+        placeholder="End"
+        onChangeText={this.updateSearch}
+        value={search}
+      />
+    );
+  }
+}
+
+export default function App() {
+  
+ return (
+   <View>
+      <StartBar style={styles.bar}> </StartBar>
+      <Destination></Destination>
+      <ImageZoom cropWidth={d.width+100}
                        cropHeight={d.height+100}
                        imageWidth={d.width+300}
                        imageHeight={d.height+100}
@@ -17,10 +66,16 @@ export default function App() {
                 <ImageBackground style={styles.image}
                        source={{uri:'https://www.mit.edu/files/images/201807/15656704711_00457bd2c9_b_1.jpg'}}/>
             </ImageZoom>
-    </View>
+  </View>
+  
+
+
+  
+
  );
 }
- 
+
+
 const styles = StyleSheet.create({
  container: {
    flex: 1,
@@ -33,7 +88,10 @@ const styles = StyleSheet.create({
   height:d.height+100,
   resizeMode: 'cover',
   position:'relative',
-
+ },
+ bar:
+ {
+   position:'absolute',
 
 
  },
