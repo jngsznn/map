08dacafa7f859fb0975c9749dd1f8420c8dc7a54
Owen <owen@duganhome.com>
2021-11-12 17:10:38 -0500

    Updated Graph

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, Dimensions, ImageBackground } from 'react-native';
-import ImageZoom from 'react-native-image-pan-zoom'
+import ImageZoom from 'react-native-image-pan-zoom';
+import Directions from './Directions.js';
 const d = Dimensions.get('window');
+
 export default function App() {
  return (
    <View style={styles.container}>
      
-        
-
-        <ImageZoom cropWidth={d.width+100}
-                       cropHeight={d.height+100}
-                       imageWidth={d.width+300}
-                       imageHeight={d.height+100}
-                       maxOverflow={200}>
-                <ImageBackground style={styles.image}
-                       source={{uri:'https://www.mit.edu/files/images/201807/15656704711_00457bd2c9_b_1.jpg'}}/>
-            </ImageZoom>
+        <ImageZoom cropWidth={d.width}
+                       cropHeight={d.height-100}
+                       imageWidth={d.width}
+                       imageHeight={d.height-100}
+                       maxOverflow={500}>
+              <ImageBackground style={styles.image}
+                       source={{uri:'https://www.mit.edu/files/images/201807/15656704711_00457bd2c9_b_1.jpg'}}
+                       style={{width: d.width, height: d.height-100}}>
+                  <Directions/>
+              </ImageBackground>
+        </ImageZoom>
     </View>
  );
 }
