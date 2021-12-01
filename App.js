d204dc0da43d23681c40c3eb709094b077dcff51
jngsznn <sjiang@mit.edu>
2021-12-01 16:07:48 -0500

    Commit

--- App.js
+++ App.js
@@ -6,41 +6,40 @@ import {SearchBar, Button} from 'react-native-elements';
 import { useState } from 'react';
 import Directions from './Directions.js';
 import Search from './Search.js';
-const d = Dimensions.get('window');
 
+const d = Dimensions.get('window');
 export default function App() {
   const [data, setData] = useState('');
 
   const updatePath = (pathData) => {
     setData(pathData);
   };
-  
+
   return (
    <View style = {styles.background}>
       <ImageZoom cropWidth={d.width}
-                       cropHeight={d.height+100}
-                       imageWidth={d.width}
-                       imageHeight={d.height}
-                       maxOverflow={500}>
-              <ImageBackground style={styles.image}
-                       source={{uri:'https://www.mit.edu/files/images/201807/15656704711_00457bd2c9_b_1.jpg'}}
-                       style={{width: d.width, height: d.height-50}}>
-                  <Directions start_end = {data}/>
-              </ImageBackground>
-        </ImageZoom>
-
-
+                      cropHeight={d.height+100}
+                      imageWidth={d.width}
+                      imageHeight={d.height}
+                      maxOverflow={500}>
+             <ImageBackground style={styles.image}
+                      source={{uri:"https://www.mit.edu/files/images/201807/15656704711_00457bd2c9_b_1.jpg"}}
+                      style={{width: d.width, height: d.height-50}}>
+                 <Directions start_end = {data}/>
+             </ImageBackground>
+       </ImageZoom>
   <SafeAreaView style={styles.container}>
     <Search style= {styles.searchbar}> </Search>
   </SafeAreaView>
   </View>
+ 
  );
 }
 
 
 const styles = StyleSheet.create({
  background: {
-  backgroundColor: 'yellow',
+  //backgroundColor: 'yellow',
  },
  container: {
    
--- Search.js
+++ Search.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, Dimensions, ImageBackground, SafeAreaView} from 'react-native';
 import ImageZoom from 'react-native-image-pan-zoom';
 import {SearchBar, Button} from 'react-native-elements';
-
 //SearchBar Class
 export default class Search extends React.Component {
     constructor(props){
       super(props);
-      this.state = {search1: "", search2: "", search_active: false, searched:false}
+      this.state = {search1: "", search2: "", search_active: false}
     }
   
     
@@ -15,8 +14,11 @@ export default class Search extends React.Component {
     updateEnd = (search2) => {this.setState({search2});};
 
 
-    find = () => {this.setState({search_active: false});};
-    find = () => {this.setState({search_active: false});};
+    find = () => {
+      this.setState({search_active: false});
+
+     };
+    
     
     render() {
       start=this.state.search1;
@@ -26,29 +28,34 @@ export default class Search extends React.Component {
         return (
         <View style={styles.container}> 
             <SearchBar
-            lightTheme="true"
+            
+            //lightTheme="true"
             placeholder= "start"
             onChangeText={this.updateStart}
             round="true"
+            inputStyle={{color:'white', fontSize:18}}
             value={this.state.search1}> 
+          
             </SearchBar>
     
     
             <SearchBar
-            lightTheme="true"
             placeholder= "end"
             round="true"
             onChangeText={this.updateEnd}
+            //containerStyle={{backgroundColor: 'white'}}
+            inputStyle={{color:'white', fontSize:18}}
             value={this.state.search2}> 
             
             </SearchBar>    
-            <Button title="Find" onPress={this.find}/>
+            <Button title="Find" styles={{fontSize:18}} onPress={this.find}/>
+            
         </View>
         );
       }
       return (
           <View style={styles.start}> 
-              <Button title="New Trip" onPress={() => 
+              <Button title="New Trip" styles={{fontSize:20}} onPress={() => 
         this.setState({"search_active": true, search1: "", search2: "" })} style={styles.start}/>
           </View>
       );
@@ -57,6 +64,7 @@ export default class Search extends React.Component {
 
    const styles = StyleSheet.create({
     container: {
+   
       
     },
     start: {
